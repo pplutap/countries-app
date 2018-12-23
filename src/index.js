1dd149ea11b9bec0d98dd9ab1d1db3685e56a1d1
Pawel Pluta <pluta.p@gmail.com>
2018-12-23 23:03:23 +0100

    Add store and Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store'
 
-
-ReactDOM.render(<h1>Empty project</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <h1>Empty project</h1>
+    </Provider>
+    , document.getElementById('root'));
