13e2c2d86fd94993165ec5a4fe009f638facd214
Pawel Pluta <pluta.p@gmail.com>
2018-12-23 23:15:44 +0100

    React devtools to verify redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
+import DevTools from './DevTools';
+import { getCountries} from "./actions/actions-countries";
 
 ReactDOM.render(
     <Provider store={store}>
-        <h1>Empty project</h1>
+        <div>
+            <h1>Empty project</h1>
+            <DevTools/>
+        </div>
     </Provider>
     , document.getElementById('root'));
+
+store.dispatch(getCountries());
